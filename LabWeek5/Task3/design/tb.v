ce2200ee72b1beb57c0dbb2c7616fb0b14eb54a5
Muhammad Shahrom Ali <estineali@gmail.com>
2019-02-15 18:44:16 +0500

    Add files via upload

---
+++ LabWeek5/Task3/design/tb.v
@@ -0,0 +1,19 @@
+module tb
+  (
+  );
+  reg[31:0] inst;
+	wire[63:0] imm_data;
+  
+  immediate_data_generator idg
+  (
+    .inst(inst),
+    .imm_data(imm_data) 
+  );
+  
+  initial
+	begin
+	inst = 32'b1011010110010111010010100100111;
+	#104 inst = 32'b1101010101101101010010101001010;
+	end 
+  
+endmodule 
