cbd01fde7f86b238d8333b37c75237689e7cbd2b
Muhammad Shahrom Ali <estineali@gmail.com>
2019-02-18 11:19:06 +0500

    Add files via upload

--- LabWeek5/Task3/design/immediate_data_generator.v
+++ LabWeek5/Task3/design/immediate_data_generator.v
@@ -1,16 +1,17 @@
 module immediate_data_generator
   (
     input[31:0] inst,
-    output[63:0] imm_data
+    output reg [63:0] imm_data
   );
+  always @ (*)
   if (inst[6] == 1'b1)
-      imm_data = {{52{inst[31]}}, inst[31], inst[7], inst[30:25], inst[11:8]};
+      assign imm_data={{52{inst[31]}}, inst[31], inst[7], inst[30:25], inst[11:8]};
   else
     begin
       if (inst[5] == 1'b1)
-        imm_data = {{52{inst[31]}}, inst[31:25], inst[11:7]};
+        assign imm_data={{52{inst[31]}}, inst[31:25], inst[11:7]};
       else
-        imm_data = {{52{inst[31]}}, inst[31:20]};  
+        assign imm_data={{52{inst[31]}}, inst[31:20]};  
     end
     
      
