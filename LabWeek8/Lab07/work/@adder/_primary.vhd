a65a932b869e1388ad9a74e589243bb56122b958
Muhammad Shahrom Ali <estineali@gmail.com>
2019-03-08 18:43:36 +0500

    Revert "Lab7 Complete"
    This reverts commit c9a480555f65e598f056219254f33d1de0f317f4.

--- LabWeek8/Lab07/work/@adder/_primary.vhd
+++
@@ -1,9 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity Adder is
-    port(
-        a               : in     vl_logic_vector(63 downto 0);
-        b               : in     vl_logic_vector(63 downto 0);
-        \out\           : out    vl_logic_vector(63 downto 0)
-    );
-end Adder;
--- LabWeek8/Lab07/work/@instruction_@fetch/_primary.vhd
+++
@@ -1,9 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity Instruction_Fetch is
-    port(
-        clk             : in     vl_logic;
-        reset           : in     vl_logic;
-        Instruction     : out    vl_logic_vector(31 downto 0)
-    );
-end Instruction_Fetch;
